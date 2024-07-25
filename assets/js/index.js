2cfe697a6a07dd80e9975678ca929021f488a28d
GoutteDooo <goudal.Donovan@live.fr>
2024-07-25 09:38:17 +0200

    version CV avec mdp3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 const button = document.querySelector(".toggleBtn");
 const page = document.querySelector(".page");
+const photo = document.querySelector(".photo");
 let navOn = true;
 
-button.addEventListener("click", () => {
+const toggleNav = () => {
   nav.classList.toggle("hideNav");
   if (navOn) {
     navOn = false;
@@ -13,14 +14,21 @@ button.addEventListener("click", () => {
     button.style.transform = "rotate(0deg)";
     button.style.opacity = 1;
   }
+};
+
+button.addEventListener("click", () => {
+  toggleNav();
 });
 
 printVersion.addEventListener("click", () => {
   page.classList.add("pagePrint");
+  toggleNav();
+  photo.style.background = "none";
   // page.classList.toggle("page");
 });
 numVersion.addEventListener("click", () => {
   page.classList.remove("pagePrint");
+  photo.style.background = "url(/assets/img/pic.JPG) center/cover";
 });
 // window.onscroll = function () {
 //   if (window.scrollY > 0) {
