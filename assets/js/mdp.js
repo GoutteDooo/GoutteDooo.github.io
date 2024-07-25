2fff9b91c0e66a8cd8510650d956f358ee59b50f
GoutteDooo <goudal.Donovan@live.fr>
2024-07-25 09:23:39 +0200

    version CV avec mdp2

--- assets/js/mdp.js
+++ assets/js/mdp.js
@@ -2,7 +2,7 @@ const password = "bonjour";
 
 enter.addEventListener("click", () => {
   if (mdp.value == "bonjour") {
-    window.location.href = "./index.html";
+    window.location.href = "./CV.html";
   } else {
     alert("le mot de passe n'est pas le bon.");
   }
