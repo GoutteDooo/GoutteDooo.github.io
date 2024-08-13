19b7b90d621f1d0bf759c0a273faad8ef70cca8f
GoutteDooo <goudal.Donovan@live.fr>
2024-08-13 15:03:20 +0200

    Page Web avec applis

--- assets/js/mdp.js
+++ assets/js/mdp.js
@@ -1,9 +1,31 @@
 const password = "bonjour";
+let nbrEssais;
+
+if (localStorage.nbrEssais > 0) {
+  nbrEssais = localStorage.nbrEssais;
+} else {
+  nbrEssais = 0;
+}
 
 enter.addEventListener("click", () => {
-  if (mdp.value == "bonjour") {
+  console.log(JSON.parse(nbrEssais));
+
+  if (mdp.value == "bonjour" && nbrEssais < 3) {
     window.location.href = "./CV.html";
+    localStorage.nbrEssais = JSON.parse(0);
+  } else if (nbrEssais >= 3) {
+    alert("Trop de tentatives ! Veuillez réessayer dans 5 minutes.");
+    setTimeout(() => {
+      nbrEssais = 0;
+      localStorage.nbrEssais = JSON.parse(0);
+    }, 300000);
   } else {
-    alert("le mot de passe n'est pas le bon.");
+    alert("le mot de passe est incorrect.");
+    nbrEssais++;
+    localStorage.nbrEssais = JSON.parse(nbrEssais);
   }
+});
+
+mdp.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") enter.click();
 });
