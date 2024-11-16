e502becbcadb2704854bfb553c6dcfca53e68b86
GoutteDooo <goudal.Donovan@live.fr>
2024-11-16 15:53:13 +0100

    add print version

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,20 +26,11 @@ if (localStorage.passwordLock == "false") {
   printVersion.addEventListener("click", () => {
     page.classList.add("pagePrint");
     toggleNav();
-    photo.style.background = "none";
     // page.classList.toggle("page");
   });
   numVersion.addEventListener("click", () => {
     page.classList.remove("pagePrint");
-    //Remettre avec la photo ---V
-    photo.style.background = "url(/assets/img/CV.JPG) center/cover";
-    photo.style.backgroundSize = "150%";
   });
-  // window.onscroll = function () {
-  //   if (window.scrollY > 0) {
-  //     nav.classList.toggle("hideNav");
-  //   }
-  // };
 } else {
   document.body.remove();
   alert("Veuillez entrer le mot de passe.");
@@ -48,13 +39,7 @@ if (localStorage.passwordLock == "false") {
 /* Copier la sélection */
 document.addEventListener("mouseup", () => {
   const selectedText = window.getSelection().toString();
-  // Comprend pas ce que j'ai fait ici, à modifier
-  lastSelectedText == selectedText
-    ? (lastSelectedText = "")
-    : (lastSelectedText = selectedText);
-  // FIN Comprend pas ce que j'ai fait ici, à modifier
-  console.log(selectedText);
-  if (selectedText != "" && lastSelectedText == selectedText)
+  if (selectedText != "" && lastSelectedText !== selectedText)
     navigator.clipboard
       .writeText(selectedText)
       .then(() => {
@@ -63,4 +48,5 @@ document.addEventListener("mouseup", () => {
       .catch((err) => {
         console.error("Erreur lors de la copie : ", err);
       });
+  lastSelectedText = selectedText;
 });
